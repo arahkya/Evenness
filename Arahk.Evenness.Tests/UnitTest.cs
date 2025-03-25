47573ad9bb54a9c677ebae28649cd6e3fb43d6b3
Arahk Yambupah <arahk@outlook.com>
2025-03-25 19:03:01 +0700

    Initial WebApi and Test Projects

---
+++ Arahk.Evenness.Tests/UnitTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Arahk.Evenness.Tests;
+
+public class UnitTest(WebApplicationFactory<Program> webApplicationFactory) : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory = webApplicationFactory;
+
+    [Fact]
+    public async Task TestGetWeatherForecast()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("weatherforecast");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("temperatureC", responseString);
+    }
+}
